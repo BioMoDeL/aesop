0d39f33feb15a9c38fe43dcb04eecb2aa36bddc6
reedharrison <rharr009@ucr.edu>
2016-01-25 13:12:39 -0800

    updated main script

--- main_alascan.py
+++ main_alascan.py
@@ -1,15 +1,33 @@
-from aesop_alascan import *
+import aesop_alascan as ala
 import pickle as p
+import prody as pd
+reload(ala)
 
 path_apbs = 'C:\\APBS\\apbs.exe'
 path_coul = 'C:\\APBS\\coulomb.exe'
-path_pdb2pqr = 'C:\\pdb2pqr-2.1.0\\pdb2pqr.exe'
+path_pdb2pqr = 'C:\\PDB2PQR\\pdb2pqr-windows-bin64-2.0.0\\pdb2pqr.exe'
 
-pdbfile = 'barnase_barstar.pdb'
+jobname = '3OXU_B_D_truncation'
+pdbfile = '3OXU_B_D.pdb'
 
-alascan = Alascan(pdbfile, path_pdb2pqr, path_apbs, coulomb_exe=path_coul, selstr=['chain A', 'chain B'], 
-	jobname='test1', region=None, grid=1, ion=0.150, pdie=20.0, sdie=78.54)
+alascan = ala.Alascan(pdbfile, path_pdb2pqr, path_apbs, coulomb_exe=path_coul, selstr=['chain B', 'chain D'],
+	jobname=jobname, region=None, grid=1, ion=0.150, pdie=20.0, sdie=78.54, ff='parse')
 
-alascan.run()
+alascan.run_truncated()
 
-p.dump(alascan, open('bn_bs_alascan.p', 'wb'))
+p.dump(alascan, open(jobname+'_alascan.p', 'wb'))
+
+# ala.plotResults(alascan, filename=jobname+'.png')
+
+
+
+
+
+
+
+
+
+# alascan = p.load(open(jobname+'_alascan.p', 'rb'))
+# import numpy as np
+# np.savetxt('mutids.txt',alascan.getMutids(), fmt='%s')
+# np.savetxt('ddGbind_rel_parse.txt', alascan.ddGbind_rel())
