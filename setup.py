26fd4b717fd3bddf688371c585a4682b5b482d7c
reedharrison <rharr009@ucr.edu>
2016-05-09 15:35:19 -0700

    added setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='aesop',
+    version='0.0.1',
+    description='Module for analyzing electrostatics with protein structures',
+    long_description=readme,
+    author='Reed Harrison, Rohith Mohan',
+    author_email='reed.harrison@email.ucr.edu, rohith.mohan@email.ucr.edu',
+    url='https://github.com/rohithmohan/aesop-python',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs', 'old'))
+)
+
