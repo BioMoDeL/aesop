164af0f8138abe7cc6d36633c2e5dd92129deea1
reedharrison <rharr009@ucr.edu>
2016-05-27 09:46:23 -0700

    Added GNU GPL license in source

--- aesop/aesop.py
+++ aesop/aesop.py
@@ -26,6 +26,33 @@ import itertools as it
 # from plotly.offline import download_plotlyjs, init_notebook_mode, iplot
 # from plotly.tools import FigureFactory as FF
 
+# Print Licencse on startup
+print("""
+AESOP: Analysis of Electrostatic Structure of Proteins
+
+Reed E. S. Harrison, Rohith R. Mohan, Dimitios Morikis
+University of California, Riverside; Department of Bioengineering
+
+Correspondence should be directed to Prof. Dimitrios Morikis at dmorikis@ucr.edu  
+
+Copyright (C) 2016  Reed E. S. Harrison, Rohith R. Mohan, Dimitios Morikis
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+""")
+    
+
+
 ######################################################################################################################################################
 # Container for performing an Alanine Scan with AESOP
 #   pdb     -   PDB file for performing Alascan. Must contain all chain selections with standard aminoacid nomenclature
