0cab95318f2cb737dbc309acbf17174fbfc37278
reedharrison <rharr009@ucr.edu>
2016-08-22 09:32:46 -0700

    adjustments

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     author_email='reed.harrison@email.ucr.edu, rohith.mohan@email.ucr.edu',
     url='https://github.com/rohithmohan/aesop-python',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs', 'old')),
-    install_requires=['prody', 'griddataformats'],
+    packages=find_packages(exclude=('tests', 'docs', 'old'))#,
+#    install_requires=['prody', 'griddataformats'],
 #    zip_safe=False	
 )
 
