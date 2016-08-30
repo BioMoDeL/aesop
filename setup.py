f9ccf142b11e8cf5f1283112b6dc34f9cfd1d52d
reedharrison <rharr009@ucr.edu>
2016-08-30 12:50:39 -0700

    version updates for pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='aesop',
-    version='0.0.10',
+    version='0.0.11',
     description='Module for analyzing electrostatics with protein structures',
     long_description=readme,
     author='Reed Harrison, Rohith Mohan',
