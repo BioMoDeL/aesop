6ad55008e749cff57f64639be437ecc3a903bf7b
reedharrison <rharr009@ucr.edu>
2017-02-06 17:22:32 -0800

    prep release 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
         return Mock()
-MOCK_MODULES = ['prody', 'modeller']
+MOCK_MODULES = ['prody', 'modeller', 'networkx']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
             
 # class Mock(object):
